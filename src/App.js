339fa44482f285bba82bd8e1596b7c3816101b1f
Shubhangi <shubhanginaik@BC-macair029s-MacBook-Air.local>
2022-03-25 14:24:07 +0200

    added Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello PS Dashboard</h1>
+        <Navbar />
       </header>
     </div>
   );

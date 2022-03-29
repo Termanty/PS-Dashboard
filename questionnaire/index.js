d64e1335314f483f6357a784e3fa6116fca233f9
Maria <bc-macair023@BCmacair023sAir.wlan>
2022-03-29 12:59:03 +0300

    hover and button colors added

--- questionnaire/index.js
+++ questionnaire/index.js
@@ -1,12 +1,10 @@
-
-function animation(){
-    const emoji= document.getElementById('emoji');
-    const btn10= document.getElementsByClassName('bt10');
-        emoji.innerText=String.fromCodePoint(0x1F495,0x1F60A);
-        emoji.classList.add('btn10animation');
-       
+function animation() {
+  const emoji = document.getElementById("emoji");
+  const btn10 = document.getElementsByClassName("bt10");
+  emoji.innerText = String.fromCodePoint(0x1f495, 0x1f60a);
+  emoji.classList.add("btn10animation");
 }
-function clearEmoji(){
-    document.getElementById('emoji').innerText='';
-    document.getElementById('emoji').classList.remove('btn10animation')
+function clearEmoji() {
+  document.getElementById("emoji").innerText = "";
+  document.getElementById("emoji").classList.remove("btn10animation");
 }

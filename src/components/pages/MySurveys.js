7ceebe8c0ab7629f2d4147391d74f0bc3821e2a6
Tero Mäntylä <termanty@gmail.com>
2022-04-12 20:47:33 +0300

    add fetch surveys feature

--- src/components/pages/MySurveys.js
+++ src/components/pages/MySurveys.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchSurveys } from "../../store/surveys/reducer";
 
 function Dashboard() {
+  const dispatch = useDispatch();
+  useEffect(() => dispatch(fetchSurveys()), []);
+
+  const surveys = useSelector((state) => state.surveys);
+  console.log(surveys);
+
+  const surveysList = surveys.map((survey) => {
+    return (
+      <li key={survey.id}>
+        <h3>{survey.name}</h3>
+        <p>{survey.question_text}</p>
+      </li>
+    );
+  });
+
   return (
     <div>
       <h1>My surveys</h1>
+      <ul>{surveysList}</ul>
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;

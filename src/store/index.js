5b260b80065e81445851f4efb111905e6c0c66c0
Tero Mäntylä <termanty@gmail.com>
2022-04-12 16:32:43 +0300

    change to use configureStore

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,10 @@
-import { combineReducers, createStore } from "redux";
-
+import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/reducer";
 import surveyReducer from "./surveys/reducer";
 
-const reducers = combineReducers({
-  user: userReducer,
-  surveys: surveyReducer,
+export const store = configureStore({
+  reducer: {
+    user: userReducer,
+    surveys: surveyReducer,
+  },
 });
-
-export const store = createStore(
-  reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);

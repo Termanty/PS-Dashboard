924f6f8d13af6937b8cb5ba9765c8005088b84d8
Tero Mäntylä <termanty@gmail.com>
2022-04-12 10:27:02 +0300

    add phz-logo

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
-import React, {Suspense, lazy} from "react";
-import {
-  BrowserRouter as
-  Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import React, { Suspense, lazy } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-const Home = lazy(() => import ("./components/pages/Home"));
+const Home = lazy(() => import("./components/pages/Home"));
 const MySurveys = lazy(() => import("./components/pages/MySurveys"));
 const Layout = lazy(() => import("./components/layout/Layout"));
-const CreateNewSurvey = lazy(() => import("./components/pages/CreateNewSurvey"));
+const CreateNewSurvey = lazy(() =>
+  import("./components/pages/CreateNewSurvey")
+);
 const Data = lazy(() => import("./components/pages/Data"));
 
 function App() {
   return (
     <div className="app">
       <Router>
-      <Suspense fallback = {<div>Loading...</div>}>
-      <Layout>
-      <Routes>
-        <Route path = "/" element = {<Home />} />
-        <Route path = "/MySurveys" element = {<MySurveys />} />
-        <Route path = "/CreateNewSurvey" element = {<CreateNewSurvey/>} />
-        <Route path = "/Data" element = {<Data />} />
-      </Routes>
-      </Layout>
-      </Suspense>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/MySurveys" element={<MySurveys />} />
+              <Route path="/CreateNewSurvey" element={<CreateNewSurvey />} />
+              <Route path="/Data" element={<Data />} />
+            </Routes>
+          </Layout>
+        </Suspense>
       </Router>
     </div>
   );
--- src/components/bars/leftSidebar/Leftbar.jsx
+++ src/components/bars/leftSidebar/Leftbar.jsx
@@ -1,21 +1,22 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import Drawer from '@mui/material/Drawer';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import DataThresholdingRoundedIcon from '@mui/icons-material/DataThresholdingRounded';
-import AutoAwesomeMosaicIcon from '@mui/icons-material/AutoAwesomeMosaic';
+import React from "react";
+import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from "./phz-logo-small.svg";
+import Drawer from "@mui/material/Drawer";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import DataThresholdingRoundedIcon from "@mui/icons-material/DataThresholdingRounded";
+import AutoAwesomeMosaicIcon from "@mui/icons-material/AutoAwesomeMosaic";
 
-import {leftbarStyle} from "./Leftbarstyle";
-import HomeIcon from './HomeIcon';
+import { leftbarStyle } from "./Leftbarstyle";
+import HomeIcon from "./HomeIcon";
 
 const drawerWidth = 260;
 
@@ -23,73 +24,103 @@ function Leftbar() {
   const classes = leftbarStyle();
 
   return (
-      <Box sx = {{ display: 'flex' }}>
+    <Box sx={{ display: "flex" }}>
       <CssBaseline />
       {/* header */}
       <AppBar
-        position = "fixed"
-        sx = {{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px`, height: "92px", backgroundColor: "transparent"}}
+        position="fixed"
+        sx={{
+          width: `calc(100% - ${drawerWidth}px)`,
+          ml: `${drawerWidth}px`,
+          height: "92px",
+          backgroundColor: "transparent",
+        }}
       >
         <Toolbar>
-          <Typography variant = "h6" noWrap component = "div" >
-             Header
+          <Typography variant="h6" noWrap component="div">
+            Header
           </Typography>
         </Toolbar>
       </AppBar>
 
       <Drawer
-        sx = {{
+        sx={{
           width: drawerWidth,
           flexShrink: 0,
-          '& .MuiDrawer-paper': {
+          "& .MuiDrawer-paper": {
             width: drawerWidth,
-            boxSizing: 'border-box',
+            boxSizing: "border-box",
             height: "100%",
             backgroundColor: "rgb(22, 38, 57)",
-            color: "white"
+            color: "white",
           },
         }}
-        variant = "permanent"
-        anchor = "left"
+        variant="permanent"
+        anchor="left"
       >
-        <h1>PHZ full stack</h1>
+        {/* <Logo /> */}
+        <Box
+          sx={{
+            height: 92,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Logo />
+        </Box>
         {/* <Toolbar /> */}
-        <Divider color = "gray"/>
+        <Divider color="gray" />
         <List>
-            <ListItem button key = "Home" component = {Link} to = "/">
-              <ListItemIcon>
-                 <HomeIcon color = "success" sx = {{ fontSize: 40}}/>
-              </ListItemIcon>
-              <ListItemText primary = "Home" />
-            </ListItem>
+          <ListItem button key="Home" component={Link} to="/">
+            <ListItemIcon>
+              <HomeIcon color="success" sx={{ fontSize: 40 }} />
+            </ListItemIcon>
+            <ListItemText primary="Home" />
+          </ListItem>
 
-            <ListItem button key = "My surveys" component = {Link} to = "/MySurveys">
-              <ListItemIcon>
-                 <AutoAwesomeMosaicIcon color = "success" sx = {{ fontSize: 40}}/>
-              </ListItemIcon>
-              <ListItemText primary = "My surveys" />
-            </ListItem>
+          <ListItem button key="My surveys" component={Link} to="/MySurveys">
+            <ListItemIcon>
+              <AutoAwesomeMosaicIcon color="success" sx={{ fontSize: 40 }} />
+            </ListItemIcon>
+            <ListItemText primary="My surveys" />
+          </ListItem>
         </List>
-        <Divider color = "gray"/>
+        <Divider color="gray" />
         <List>
-          <ListItem button key = "Data" component = {Link} to = "/Data">
-              <ListItemIcon>
-                 <DataThresholdingRoundedIcon color = "success" sx = {{ fontSize: 40}}/>
-              </ListItemIcon>
-              <ListItemText primary = "Data" />
-            </ListItem>
+          <ListItem button key="Data" component={Link} to="/Data">
+            <ListItemIcon>
+              <DataThresholdingRoundedIcon
+                color="success"
+                sx={{ fontSize: 40 }}
+              />
+            </ListItemIcon>
+            <ListItemText primary="Data" />
+          </ListItem>
 
-            <ListItem sx = {{marginTop: "50px", textAlign: "center"}} button className = {classes.addButton} key = {"Create new survey"} component = {Link} to = "/CreateNewSurvey">
-              <ListItemText primary = {"Create new survey"} />
-            </ListItem>
+          <ListItem
+            sx={{ marginTop: "50px", textAlign: "center" }}
+            button
+            className={classes.addButton}
+            key={"Create new survey"}
+            component={Link}
+            to="/CreateNewSurvey"
+          >
+            <ListItemText primary={"Create new survey"} />
+          </ListItem>
 
-            <ListItem sx = {{marginTop: "150px"}} button className = {classes.logoutButton} key = {"Logout"}>
-              <ListItemText primary = {"Logout"} sx = {{textAlign: "center"}}/>
-            </ListItem>
+          <ListItem
+            sx={{ marginTop: "150px" }}
+            button
+            className={classes.logoutButton}
+            key={"Logout"}
+          >
+            <ListItemText primary={"Logout"} sx={{ textAlign: "center" }} />
+          </ListItem>
         </List>
       </Drawer>
-     </Box>
-  )
+    </Box>
+  );
 }
 
 export default Leftbar;

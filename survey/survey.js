a9a9cb2a26c81bf3cf8fcfbbf21fbe7d8c20fab7
FemiAdesola <75606768+FemiAdesola@users.noreply.github.com>
2022-04-18 07:26:25 +0300

    Change survey  background color random

--- survey/survey.js
+++ survey/survey.js
@@ -1,3 +1,54 @@
+ const colors = [
+  "rgb(25,154,216)",
+  "rgb(196,4,114)",
+  "rgb(180,27,255)",
+  "rgb(60,33,191)",
+  "rgb(34,116,246)",
+  "rgb(140,212,38)",
+  "rgb(238,4,181)",
+  "rgb(159,77,22)",
+  "rgb(9,189,57)",
+  "rgb(43,114,187)",
+  "rgb(118,186,97)",
+  "rgb(181,69,131)",
+  "rgb(120,173,7)",
+  "rgb(12,36,79)",
+  "rgb(222,71,163)",
+  "rgb(133,121,111)",
+  "rgb(89,176,171)",
+  "rgb(48,159,231)",
+  "rgb(3,132,250)",
+  "rgb(3,181,69)",
+  "rgb(48,209,103)",
+  "rgb(154,129,248)",
+  "rgb(157,203,10)",
+  "rgb(32,245,27)",
+  "rgb(58,61,118)",
+  "rgb(225,112,85)",
+  "rgb(138,83,191)",
+  "rgb(74,205,250)",
+  "rgb(71,14,238)",
+  "rgb(129,164,18)",
+  "rgb(130,125,190)",
+  "rgb(90,144,230)",
+  "rgb(40,64,239)",
+  "rgb(56,107,55)"
+]
+
+function getColor() {
+  const Color=  ()=>{
+    // let Random = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    // return Random
+
+    // let colors = ['#D2D9EC', 'green', 'blue', '#162639'];    
+    let Random = colors[Math.floor(Math.random() * colors.length)];
+    return Random 
+  };
+  // document.body.style.backgroundColor = Color()
+  document.getElementById('main').style.backgroundColor = Color()
+}
+getColor()
+
 function animation() {
   const emoji = document.getElementById("emoji");
   const btn10 = document.getElementsByClassName("bt10");

e80f72b9ca5485b3484ca94203d555378fc93c09
Shubhangi <shubhanginaik@BC-macair029s-MacBook-Air.local>
2022-05-01 18:20:23 +0300

    added table format and link to the data page

---
+++ src/components/pages/Pages.style.js
@@ -0,0 +1,22 @@
+import { makeStyles } from "@mui/styles";
+import { borderRadius } from "@mui/system";
+
+export const PagesStyle = makeStyles({
+  mySurvey: {
+    fontSize: "16px"
+  },
+  openButton: {
+    width: "120px",
+    height: "40px",
+    backgroundColor: "#D66434",
+    borderRadius: "25px",
+  },
+  circle: {
+    width: "15px",
+    height: "15px",
+    borderRadius: "50%",
+    backgroundColor: "green",
+    marginLeft: "10px",
+    marginTop:"5px"
+  }
+})

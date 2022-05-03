a97ebeaf01c6f8f6f072e7582e40eb6e6c374f19
Tero Mäntylä <termanty@gmail.com>
2022-05-03 20:43:27 +0300

    response count by date

--- src/components/pages/Data.js
+++ src/components/pages/Data.js
@@ -1,13 +1,23 @@
-import {Box} from '@mui/material';
-import {Card} from '@mui/material';
+import { Box } from "@mui/material";
+import { Card } from "@mui/material";
 
-import {Typography} from '@mui/material';
+import { Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchResponses } from "../../store/responses/reducer";
-import Plot from 'react-plotly.js';
-import moment from 'moment';
+import Plot from "react-plotly.js";
+import moment from "moment";
 
+function reducer(accumulator, day) {
+  if (!accumulator[day]) accumulator[day] = 0;
+  accumulator[day]++;
+  return accumulator;
+}
+
+function toTime(response) {
+  const time = response.created_at;
+  return time.slice(0, 10).split("-").join("");
+}
 
 function Data() {
   const dispatch = useDispatch();
@@ -15,249 +25,268 @@ function Data() {
   useEffect(() => dispatch(fetchResponses()), []);
 
   const responses = useSelector((state) => state.responses);
-  // const responsesList = responses.map((response) => (
-  //   <li key={response.id}>
-  //     <p>
-  //       score: {response.score}, comment: {response.comment}
-  //     </p>
 
+  const responsesPerDay = responses.map(toTime).reduce(reducer, {});
+  const detractorsPerDay = responses
+    .filter((r) => r.score <= 6)
+    .map(toTime)
+    .reduce(reducer, {});
+  const passivesPerDay = responses
+    .filter((r) => r.score >= 7 && r.score <= 8)
+    .map(toTime)
+    .reduce(reducer, {});
+  const promotersPerDay = responses
+    .filter((r) => r.score >= 9)
+    .map(toTime)
+    .reduce(reducer, {});
 
-  //   </li>
-  // ));
+  console.log(responsesPerDay);
+  console.log(detractorsPerDay);
+  console.log(passivesPerDay);
+  console.log(promotersPerDay);
 
-const timeStamp= [];
-for (let i = 0; i < responses.length; i += 1) {
-  timeStamp.push(new Date(responses[i].created_at).toLocaleDateString());
-}
-console.log(timeStamp)
+  let detractor = 0;
+  let promoter = 0;
+  let passive = 0;
 
+  for (let i = 0; i < responses.length; i++) {
+    if (responses[i].score >= 9) promoter++;
+    if (responses[i].score >= 7 && responses[i].score <= 8) ++passive;
+    if (responses[i].score <= 6) detractor++;
+  }
 
-let detractor=0
-let promoter = 0;
-let passive = 0
+  let PR = promoter++;
+  let DE = detractor++;
+  let PA = passive++;
 
-  for (let i=0; i< responses.length;  i++){
-    if (responses[i].score>=9) promoter++;
-    if (responses[i].score>=7 && responses[i].score <=8) ++ passive;
-    if(responses[i].score <=6) detractor++;
-  };
+  const All = PR + DE + PA;
 
-let PR= promoter++
-let DE = detractor++
-let PA = passive++
+  const Promoters = Math.round((PR / All) * 100);
+  const Detractors = Math.round((DE / All) * 100);
+  const Passives = Math.round((PA / All) * 100);
+  const NPScore = Math.round(Promoters - Detractors);
 
-const All = PR+DE+PA
+  const NPS = Math.min(Math.max(parseInt(NPScore), -100), 100);
 
-const Promoters= Math.round((PR/All)*100)
-const Detractors = Math.round((DE/All)*100)
-const Passives= Math.round((PA/All)*100)
-const NPScore= Math.round((Promoters - Detractors))
-
-const NPS = Math.min(Math.max(parseInt(NPScore),-100),100);
-
-const [userData, setUserData]= useState({
-  labels:["Detractors", "Passives", "Promoters"],
-  datasets:[{
-    label:"Net Promoter Score",
-    data:[Detractors,Passives,Promoters],
-    borderWidth: 0,
-    backgroundColor: [
-      '#E26060',
-      '#F3C934',
-      '#52A569',
+  const [userData, setUserData] = useState({
+    labels: ["Detractors", "Passives", "Promoters"],
+    datasets: [
+      {
+        label: "Net Promoter Score",
+        data: [Detractors, Passives, Promoters],
+        borderWidth: 0,
+        backgroundColor: ["#E26060", "#F3C934", "#52A569"],
+        hoverOffset: 4,
+        circumference: 180,
+        rotation: -90,
+        options: {
+          legend: "bottom",
+          font: {
+            size: 50,
+          },
+        },
+      },
     ],
-    hoverOffset: 4,
-    circumference:180,
-    rotation:-90,
-    options:{
-      legend:"bottom",
-      font: {
-        size: 50
-    },
-
-    }
-  },
-
-
-],
-
-},
-);
+  });
 
   return (
     <div>
-       <Typography
-        variant="h4"
-        component="div"
-        sx={{m:3}}>
-          NET PROMOTER SCORE
-        </Typography>
+      <Typography variant="h4" component="div" sx={{ m: 3 }}>
+        NET PROMOTER SCORE
+      </Typography>
 
       {/* <ul>{responsesList}</ul> */}
 
-
-     <Box  sx={{ marginLeft: 27}}>
-      <Card sx={{ maxWidth: 350, m:3,
-        display: 'flex',
-        background:"#E5E5E5",
-        '&:hover': {
-        backgroundColor: 'white'},
-      }}
-      >
-        <Typography
-        align = "center"
-        variant="h4"
-        component="div"
-        sx={{color:"black",
-        m:2, width:600,
-
-        }}
-
-        > {`Total Responses ${All}`}
-        </Typography>
-      </Card>
-     </Box>
-
-
-    <Box
-        sx={{ display: 'flex', flexDirection: 'row',   height:250, marginBottom:10 }}>
-
-      <Card sx={{
-            maxWidth: 220,
-            m:3, color:"#E26060",
-            background:"#E5E5E5",
-            '&:hover': {
-              backgroundColor: 'white'}
-          }} >
-              <Typography
-                  sx={{color:"black",
-                  m:3, width:350,
-                  marginLeft:-3,
-                  paddingRight:10}}
-                  align = "center"
-                  variant="h4"
-                  component="div" >
-                    Detractors %
-              </Typography>
-              <Typography
-                align = "center"
-                variant="h3"
-                sx={{fontSize: 60, fontWeight:"bold"}}
-                component="div"
-                marginTop={3} >
-                {`${Detractors}`}
-              </Typography>
-      </Card>
-
-      <Card sx={{
-        maxWidth: 220,
-        m:3,
-        background:"#E5E5E5",
-        color:"#F3C934",
-        '&:hover': {
-            backgroundColor: 'white'}
-      }}
-      >
+      <Box sx={{ marginLeft: 27 }}>
+        <Card
+          sx={{
+            maxWidth: 350,
+            m: 3,
+            display: "flex",
+            background: "#E5E5E5",
+            "&:hover": {
+              backgroundColor: "white",
+            },
+          }}
+        >
           <Typography
-            sx={{color:"black",
-            m:3, width:350,
-            marginLeft:-3,
-            paddingRight:10,
-           }}
-            align = "center"
+            align="center"
             variant="h4"
-            component="div" >
-              Passives %
+            component="div"
+            sx={{ color: "black", m: 2, width: 600 }}
+          >
+            {" "}
+            {`Total Responses ${All}`}
+          </Typography>
+        </Card>
+      </Box>
+
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          height: 250,
+          marginBottom: 10,
+        }}
+      >
+        <Card
+          sx={{
+            maxWidth: 220,
+            m: 3,
+            color: "#E26060",
+            background: "#E5E5E5",
+            "&:hover": {
+              backgroundColor: "white",
+            },
+          }}
+        >
+          <Typography
+            sx={{
+              color: "black",
+              m: 3,
+              width: 350,
+              marginLeft: -3,
+              paddingRight: 10,
+            }}
+            align="center"
+            variant="h4"
+            component="div"
+          >
+            Detractors %
           </Typography>
           <Typography
-          align = "center"
-          variant="h3"
-          sx={{fontSize: 60, fontWeight:"bold"}}
-          component="div"
-          marginTop={3} >
-          {` ${Passives}`}
+            align="center"
+            variant="h3"
+            sx={{ fontSize: 60, fontWeight: "bold" }}
+            component="div"
+            marginTop={3}
+          >
+            {`${Detractors}`}
           </Typography>
+        </Card>
 
-      </Card>
-
-      <Card sx={{
-        maxWidth: 220,
-        m:3,
-        color:"#52A569",
-        background:"#E5E5E5",
-        '&:hover': {
-            backgroundColor: 'white'},
-
-      }} >
+        <Card
+          sx={{
+            maxWidth: 220,
+            m: 3,
+            background: "#E5E5E5",
+            color: "#F3C934",
+            "&:hover": {
+              backgroundColor: "white",
+            },
+          }}
+        >
           <Typography
-          sx={{color:"black",
-          m:3, width:350,
-          marginLeft:-3,
-          paddingRight:10}}
-          align = "center"
-          variant="h4"
-          component="div" >
+            sx={{
+              color: "black",
+              m: 3,
+              width: 350,
+              marginLeft: -3,
+              paddingRight: 10,
+            }}
+            align="center"
+            variant="h4"
+            component="div"
+          >
+            Passives %
+          </Typography>
+          <Typography
+            align="center"
+            variant="h3"
+            sx={{ fontSize: 60, fontWeight: "bold" }}
+            component="div"
+            marginTop={3}
+          >
+            {` ${Passives}`}
+          </Typography>
+        </Card>
+
+        <Card
+          sx={{
+            maxWidth: 220,
+            m: 3,
+            color: "#52A569",
+            background: "#E5E5E5",
+            "&:hover": {
+              backgroundColor: "white",
+            },
+          }}
+        >
+          <Typography
+            sx={{
+              color: "black",
+              m: 3,
+              width: 350,
+              marginLeft: -3,
+              paddingRight: 10,
+            }}
+            align="center"
+            variant="h4"
+            component="div"
+          >
             Promoters %
           </Typography>
           <Typography
-          align = "center"
-          variant="h3"
-          sx={{fontSize: 60, fontWeight:"bold"}}
-          marginTop={3} >
-          {` ${Promoters} `}
+            align="center"
+            variant="h3"
+            sx={{ fontSize: 60, fontWeight: "bold" }}
+            marginTop={3}
+          >
+            {` ${Promoters} `}
           </Typography>
-      </Card>
-      <Card sx={{
-        borderRadius:"50%",
-        maxWidth: 200,
-        m:3,
-        background:"#E5E5E5",
-        '&:hover': {
-          backgroundColor: 'white'}}}
-      >
-        <Typography
-          sx={{color:"black",
-          m:5,
-          width:120,
-          paddingRight:2}}
-          align = "center"
-          variant="h4"
-          component="div" >
+        </Card>
+        <Card
+          sx={{
+            borderRadius: "50%",
+            maxWidth: 200,
+            m: 3,
+            background: "#E5E5E5",
+            "&:hover": {
+              backgroundColor: "white",
+            },
+          }}
+        >
+          <Typography
+            sx={{ color: "black", m: 5, width: 120, paddingRight: 2 }}
+            align="center"
+            variant="h4"
+            component="div"
+          >
             NPS
-        </Typography>
-        <Typography
-          align = "center"
-          variant="h2"
-          sx={{fontSize: 60, fontWeight:"bold"}}
-          component="div"
-          marginTop={-3}  >
-          {` ${NPS}`}
-        </Typography>
-      </Card>
-    </Box>
-    <Box sx={{ display: 'flex'}}>
-      <Plot
+          </Typography>
+          <Typography
+            align="center"
+            variant="h2"
+            sx={{ fontSize: 60, fontWeight: "bold" }}
+            component="div"
+            marginTop={-3}
+          >
+            {` ${NPS}`}
+          </Typography>
+        </Card>
+      </Box>
+      <Box sx={{ display: "flex" }}>
+        <Plot
           data={[
             {
-              values: [Detractors ,Passives, Promoters],
+              values: [Detractors, Passives, Promoters],
               labels: ["Detractors", "Passives", "Promoters"],
-              domain: {column: 0},
-              hoverinfo: 'label+percent',
-              rotation:90,
-              marker:{
-                colors: [
-                  '#CE672E',
-                  '#F3C934',
-                  '#00ff00'],
+              domain: { column: 0 },
+              hoverinfo: "label+percent",
+              rotation: 90,
+              marker: {
+                colors: ["#CE672E", "#F3C934", "#00ff00"],
               },
-              textposition: 'inside',
-              hole: .4,
-              type: 'pie',
-
+              textposition: "inside",
+              hole: 0.4,
+              type: "pie",
             },
-            ]}
-            layout={ { height:500, width: 550, title: 'Net Promoter Score',
-            font:{
+          ]}
+          layout={{
+            height: 500,
+            width: 550,
+            title: "Net Promoter Score",
+            font: {
               size: 20,
             },
 
@@ -265,64 +294,67 @@ const [userData, setUserData]= useState({
               x: 0.1,
               y: -0.1,
               orientation: "h",
-              font:{
-                size: 15
+              font: {
+                size: 15,
               },
-              bgcolor: '#E2E2E2',
-              bordercolor: '#FFFFFF',
+              bgcolor: "#E2E2E2",
+              bordercolor: "#FFFFFF",
               borderwidth: 1,
-              borderradius:1,
+              borderradius: 1,
             },
             annotations: [
               {
                 font: {
-                  size: 20
+                  size: 20,
                 },
                 showarrow: false,
-                text:`NPS`,
+                text: `NPS`,
                 x: 0.5,
-                y: 0.55
+                y: 0.55,
               },
-                {
-                  font: {
-                    size: 40
-                  },
-                  showarrow: false,
-                  text:`${NPS}`,
-                  x: 0.5,
-                  y: 0.45
-                }
-            ]
+              {
+                font: {
+                  size: 40,
+                },
+                showarrow: false,
+                text: `${NPS}`,
+                x: 0.5,
+                y: 0.45,
+              },
+            ],
           }}
-      />
+        />
 
-      <Plot
+        <Plot
           data={[
             {
-              y: [Detractors ,Passives, Promoters],
+              y: [Detractors, Passives, Promoters],
               x: ["Detractors", "Passives", "Promoters"],
-              text: [[`${Detractors} %`],[`${Passives} %`],[`${Promoters} %`]].map(String),
-              hoverinfo: 'label+percent',
-              marker:{
-                color:[
-                  '#ED6930',
-                  '#ED6930',
-                  '#ED6930'
-                ],
+              text: [
+                [`${Detractors} %`],
+                [`${Passives} %`],
+                [`${Promoters} %`],
+              ].map(String),
+              hoverinfo: "label+percent",
+              marker: {
+                color: ["#ED6930", "#ED6930", "#ED6930"],
                 opacity: 0.9,
-                size:10
+                size: 10,
               },
-              type: 'bar'
+              type: "bar",
             },
-            ]}
-          layout={ {height:500, width: 500, title: 'Net Promoter Score',  font:{
-            size: 20
+          ]}
+          layout={{
+            height: 500,
+            width: 500,
+            title: "Net Promoter Score",
+            font: {
+              size: 20,
             },
-          } }
-
-      />
-    </Box>
-  </div>
+          }}
+        />
+      </Box>
+    </div>
   );
 }
 

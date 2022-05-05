39f6be187e4d389ddf137cd332fd520389e4bd0d
Shubhangi <shubhanginaik@BC-macair029s-MacBook-Air.local>
2022-05-05 15:30:39 +0300

    changed search position for mysurvey and added search functionality

--- src/components/pages/MySurveys.js
+++ src/components/pages/MySurveys.js
@@ -1,7 +1,6 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchSurveys } from "../../store/surveys/reducer";
-
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -11,9 +10,13 @@ import TableRow from '@mui/material/TableRow';
 import Button from '@mui/material/Button';
 import { PagesStyle } from './Pages.style.js';
 import moment from "moment";
-
+import OutlinedInput from '@mui/material/OutlinedInput';
+import Paper from "@mui/material/Paper";
+import SearchIcon from '@mui/icons-material/Search';
+import InputAdornment from "@mui/material/InputAdornment";
 
 function Dashboard() {
+  const [searchedSurvey, setSearchedSurvey] = useState("");
   const dispatch = useDispatch();
   const classes = PagesStyle();
   useEffect(() =>
@@ -32,7 +35,19 @@ function Dashboard() {
   });
 
   return (
-    <TableContainer sx={{marginTop:5}}>
+    <>
+    <Paper sx={{height: 100, justifyContent: "center", textAlign: "center", paddingTop: 3}}>
+      <>
+    <OutlinedInput
+      type = "text"
+       placeholder = "Search survey name..."
+       sx = {{ width: 500, height: 55, backgroundColor: "#F3F6F9", borderRadius: 5 }}
+       onChange = {(e)=>setSearchedSurvey(e.target.value)}
+      />
+      </>
+      </Paper>
+      <Paper>
+    <TableContainer sx={{marginTop:10}}>
       <Table sx={{minWidth: 650}}>
       <TableHead>
         <TableRow>
@@ -59,7 +74,8 @@ function Dashboard() {
         </TableRow>
         </TableHead>
         <TableBody>
-          {surveys.map((survey) => (
+          { !searchedSurvey ?
+          surveys.map((survey) => (
             <TableRow
             key={ survey.id }
             sx={{ '&:last-child td, &:last-child th': {border: 0} }}
@@ -79,10 +95,37 @@ function Dashboard() {
               <Button className={classes.openButton} key={survey.id} variant = "contained" href = {`/${survey.name}`}>Open</Button>
               </TableCell>
             </TableRow>
-          ))}
+          )):
+          surveys.filter((srv) => srv.name.toLowerCase()
+          .includes(searchedSurvey.toLowerCase()
+          ))
+          .map((survey) => (
+            <TableRow
+            key={ survey.id }
+            sx={{ '&:last-child td, &:last-child th': {border: 0} }}
+            >
+              <TableCell component = "th" scope = "row" sx={{borderBottom: "none", fontSize:18}}>
+                {survey.name}
+              </TableCell>
+              <TableCell component = "th" scope = "row" sx={{borderBottom: "none", fontSize:18}}>
+                {moment(survey.created_at).utc().format('DD.MM.YYYY')}
+              </TableCell>
+              <TableCell component = "th" scope = "row" sx={{borderBottom: "none", fontSize:18}}>
+                <div style={{display: "flex"}}>
+                {"Ongoing"}<div className={classes.circle} />
+                </div>
+              </TableCell>
+              <TableCell component = "th" scope = "row" sx={{borderBottom: "none", fontSize:18}}>
+              <Button className={classes.openButton} key={survey.id} variant = "contained" href = {`/${survey.name}`}>Open</Button>
+              </TableCell>
+            </TableRow>
+          ))
+        }
         </TableBody>
       </Table>
     </TableContainer>
+    </Paper>
+    </>
   );
 }
 

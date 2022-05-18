10c844cde78bfef978a7e6ffa7931afe858f7988
Shubhangi <shubhanginaik@BC-macair029s-MacBook-Air.local>
2022-05-18 12:34:08 +0300

    added embed tab

--- src/components/pages/Embed.js
+++ src/components/pages/Embed.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import PropTypes from 'prop-types';
-import {CopyToClipboard} from 'react-copy-to-clipboard';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import {
   Tabs,
   Tab,
@@ -43,26 +43,28 @@ function a11yProps(index) {
     'aria-controls': `simple-tabpanel-${index}`,
   };
 }
+
 function Embed() {
   const [value, setValue] = useState(0);
   const [copied, setCopied] = useState(false);
+  const [scriptCopied, setScriptCopied] = useState(false);
+
   const handleTabs = (e, val) => {
     setValue(val);
   }
 
   return (
     <>
-      {/* <Box sx={{ width: '100%', marginTop: '50px' }}> */}
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider', marginTop: 5 }}>
         <Tabs
         value={value}
         onChange={ handleTabs }
         aria-label="embed code"
         >
           <Tab label='Direct Link' { ...a11yProps(0) }
-          sx={{ marginRight: "40%", fontSize: 18, textTransform: 'none' }}/>
-          <Tab label="WordPress embed" {...a11yProps(1)}
-          sx={{ fontSize: 18,textTransform: 'none' }}
+          sx={{ marginRight: "40%",marginLeft: "7%", fontSize: 20, textTransform: 'none' }}/>
+          <Tab label="Embed Script" {...a11yProps(1)}
+          sx={{ fontSize: 20, textTransform: 'none' }}
           />
         </Tabs>
       </Box >
@@ -88,7 +90,12 @@ function Embed() {
           >
           <Button
           sx={{width:100, height: 45, marginTop: 1,marginLeft:.1,
-            color: "white", backgroundColor:"rgb(22, 38, 57)"}}
+            color: "white", backgroundColor:"rgb(22, 38, 57)",
+            "&:hover": {
+              backgroundColor: "rgb(80, 50, 90)",
+              opacity: [0.4, 0.35, 0, 7],
+            },
+          }}
           >
             Copy
             <FileCopyOutlinedIcon sx={{marginLeft: 2, color: "white"}}/>
@@ -102,13 +109,52 @@ function Embed() {
       <TabPanel value={value} index={1}>
       <Box sx={{ color: "black" }}>
       <Typography>
-            <div style={{ fontSize: 20, marginBottom: 5}}>Embed the survey in WordPress</div>
-            <div style={{ fontSize: 18,marginBottom: 20, color: "rgb(53,126,199)" }}>If you manage a WordPress site, you can down our plugin and install it on your website</div>
+            <div style={{ fontSize: 20, marginBottom: 5}}>Embed survey to your website
+            </div>
+            <div style={{ fontSize: 18,marginBottom: 20, color: "rgb(53,126,199)" }}>Copy this script and embed on your website
+            </div>
           </Typography>
+          <Divider />
+          <div style={{display: "inline-flex", marginTop: 60, marginBottom: 20}}>
+          <textarea
+          style = {{width: 900, fontSize: 20}}
+          id = "embed-script"
+          rows= "5"
+          defaultValue="<div class='happy-survey-wrapper'>
+          <div id='hs-embeded-survey' class='hs-embeded-survey'>
+          </div>
+          <script
+            type='text/javascript'
+            src='./hs-emb.js'
+            id='happy-survey-script'
+            surveyId='b1e5ff6a-6337-4616-b8e0-a9a6e388c6f6'
+          ></script>
+        </div>"
+          readOnly
+          />
+          <CopyToClipboard
+          text = "<div class='happy-survey-wrapper'><div id='hs-embeded-survey' class='hs-embeded-survey'></div><script type='text/javascript' src='./hs-emb.js' id='happy-survey-script' surveyId='b1e5ff6a-6337-4616-b8e0-a9a6e388c6f6'></script></div>"
+          onCopy = {() => setScriptCopied(true)}
+          >
+          <Button
+          sx={{
+            width:100, height: 45, marginTop: 1,marginLeft:.1,
+            color: "white", backgroundColor:"rgb(22, 38, 57)",
+            "&:hover": {
+              backgroundColor: "rgb(80, 50, 90)",
+              opacity: [0.4, 0.35, 0, 7],
+            },
+          }}
+          >
+            Copy
+            <FileCopyOutlinedIcon sx={{marginLeft: 2, color: "white"}}/>
+            </Button>
+          </CopyToClipboard>
+          {scriptCopied ? <span style={{color: 'red', marginTop: 14}}>Copied.</span> : null}
+          </div>
           </Box>
       </TabPanel>
       </Paper>
-    {/* </Box> */}
     </>
   )
 }

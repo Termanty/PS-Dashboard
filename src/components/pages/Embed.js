322095728fbb196f9114a2b06b0492063fbac683
Shubhangi <shubhanginaik@BC-macair029s-MacBook-Air.local>
2022-05-18 12:38:20 +0300

    changed script tag

--- src/components/pages/Embed.js
+++ src/components/pages/Embed.js
@@ -122,18 +122,18 @@ function Embed() {
           rows= "5"
           defaultValue="<div class='happy-survey-wrapper'>
           <div id='hs-embeded-survey' class='hs-embeded-survey'>
-          </div>
           <script
             type='text/javascript'
             src='./hs-emb.js'
             id='happy-survey-script'
             surveyId='b1e5ff6a-6337-4616-b8e0-a9a6e388c6f6'
           ></script>
+          </div>
         </div>"
           readOnly
           />
           <CopyToClipboard
-          text = "<div class='happy-survey-wrapper'><div id='hs-embeded-survey' class='hs-embeded-survey'></div><script type='text/javascript' src='./hs-emb.js' id='happy-survey-script' surveyId='b1e5ff6a-6337-4616-b8e0-a9a6e388c6f6'></script></div>"
+          text = "<div class='happy-survey-wrapper'><div id='hs-embeded-survey' class='hs-embeded-survey'><script type='text/javascript' src='./hs-emb.js' id='happy-survey-script' surveyId='b1e5ff6a-6337-4616-b8e0-a9a6e388c6f6'></script></div></div>"
           onCopy = {() => setScriptCopied(true)}
           >
           <Button
